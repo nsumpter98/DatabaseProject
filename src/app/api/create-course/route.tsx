a03a0ea7c126e5f22046f5590c731ed94f6e8a80
Nathanael Sumpter <nsumpter98@gmail.com>
2024-02-27 13:54:02 -0500

    testing2

--- src/app/api/create-course/route.tsx
+++ src/app/api/create-course/route.tsx
@@ -1,12 +1,10 @@
 // pages/api/courses/create.ts
 import { prisma } from '../../../../db'; // Adjust the import path as needed
 import { NextRequest, NextResponse } from 'next/server';
-import axios from "axios";
 
 export async function POST(req: NextRequest) {
     try {
         const { courseName, topic } = await req.json();
-        const result = axios.get("http://database_project_express:80/");
 
         // Create a course
         const newCourse = await prisma.course.create({
@@ -16,7 +14,7 @@ export async function POST(req: NextRequest) {
             },
         });
 
-        return NextResponse.json({response: newCourse, testing: result}, {status: 200});
+        return NextResponse.json({response: newCourse}, {status: 200});
     } catch (error) {
         console.error("Failed to create course: ", error);
         return NextResponse.json({response: "fail"}, {status: 500});
