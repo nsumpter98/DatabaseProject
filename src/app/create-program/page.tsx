f72c1d98a666b31d914f6e1b626268f365407110
Nathanael Sumpter <nsumpter98@gmail.com>
2024-02-28 09:58:33 -0500

    corrected button name for program

--- src/app/create-program/page.tsx
+++ src/app/create-program/page.tsx
@@ -47,7 +47,7 @@ export default function CreateProgram() {
                 </div>
                 <button type="submit"
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                    Create Course
+                    Create Program
                 </button>
             </form>
         </div>
