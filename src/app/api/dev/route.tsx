486b1c13285389c270b07b449876ed83aef3cd48
Nathanael Sumpter <nsumpter98@gmail.com>
2024-03-01 13:21:07 -0500

    development

---
+++ src/app/api/dev/route.tsx
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+import axios from "axios";
+
+export async function GET(req: NextRequest) {
+    try {
+
+
+        const response = await axios.get('http://projectname_service:80/');
+
+
+        return NextResponse.json({response: response}, {status: 200});
+    } catch (error) {
+        console.error("Failed to create course: ", error);
+        return NextResponse.json({response: error}, {status: 500});
+    }
+}
---
+++ src/app/dev/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function CreatePerson() {
+
+    //GET request to /api/dev
+    const [response, setResponse] = useState('');
+    const [error, setError] = useState('');
+
+    const getDev = async (e: any) => {
+        e.preventDefault();
+        try {
+            const response = await axios.get('/api/dev');
+            console.log(response);
+            setResponse(response.data);
+        } catch (error: any) {
+            console.error(error);
+            setError(error);
+        }
+    };
+
+    return (
+        <div className="max-w-sm mx-auto mt-10">
+            <h1 className="text-2xl font-bold mb-5 text-center">Check</h1>
+            <form onSubmit={getDev} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                        Get Dev
+                    </button>
+                </div>
+                <div className="mb-4">
+                    <p>{response.toString()}</p>
+                </div>
+                <div className="mb-4">
+                    <p>{error.toString()}</p>
+                </div>
+            </form>
+
+        </div>
+
+
+    );
+}
